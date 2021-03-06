0b396153b95c83358127af113b7d8b38b176d189
Samreis274 <samreis274@users.noreply.github.com>
2021-03-06 19:48:26 -0300

    Update

---
+++ Assets/Script/Continua.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Continua : MonoBehaviour
+{
+    public Transform inicio;
+    public List<Transform> lista = new List<Transform>();
+    Vector3 direction;
+    public float speed;
+    int contador;
+    bool teste;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Vector3.Distance(inicio.position, lista[contador].position) > 0.02)
+        {
+            direction = lista[contador].position - inicio.position;
+            inicio.Translate(direction.normalized * speed * Time.deltaTime);
+            
+        }
+        else
+        {
+          
+            if (teste)
+            {
+                if (contador == 0)
+                {
+                    teste = false;
+                }
+                else
+                {
+                    contador--;
+                }
+                
+            }
+            else
+            {   
+                if (contador>=lista.Count-1)
+                {
+                    teste = true;
+                }
+                else
+                {
+                    contador++;
+                }
+                
+            }  
+
+        }
+    }
+}
---
+++ Assets/Script/Inimigo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inimigo : MonoBehaviour
+{
+   public Transform inicio;
+    public Transform fim;
+    Vector3 direction;
+    public float speed;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(inicio.position,fim.position) > 0.02)
+        {
+            direction = fim.position - inicio.position;
+            inicio.Translate(direction.normalized * speed *Time.deltaTime ); 
+        }
+
+
+        print(Time.deltaTime);
+    }
+}
